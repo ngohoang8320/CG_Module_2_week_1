2f27d2ddb2037ba33f48689014cec465eff997c1
Ngo Hoang <ngohoang8320@gmail.com>
2024-01-16 16:43:36 +0700

    Add practice project named static-property-test.

---
+++ Access_modifier_and_Static/static-property-test/src/Car.java
@@ -0,0 +1,27 @@
+public class Car {
+    public static int numberOfCars;
+    private String name;
+    private String engine;
+
+    public Car(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+}
